064e72b11e5e141c99f06eabb8e62b7af143895d
José Sancho <jsancho.dev@gmail.com>
2021-02-13 19:10:03 +0100

    Navigation to note detail implemented

---
+++ Notebooks/Notebooks/NoteDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NoteDetailViewController.swift
+//  Notebooks
+//
+//  Created by José Sancho on 13/2/21.
+//
+
+import UIKit
+
+class NoteDetailViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
